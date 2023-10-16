23a8a44311a3259bd8aaeafff4622f1438f082fb
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2023-10-16 14:08:24 +0200

    Update Quizz.cpp

--- Quizz.cpp
+++ Quizz.cpp
@@ -10,26 +10,68 @@ void Startup() {
 
 
 
+//Theme 1, culture general
+
 void Game1() {
 	int score = 0; // Score initial
 
 	std::cout << "Bienvenue au Quizz de Culture Generale !" << std::endl;
 	std::cout << "Repondez aux questions en choisissant la bonne option (1, 2 ou 3)." << std::endl;
 
-	// Question 1
-	std::cout << "\n Quel est le premier satellite envoyer dans l'espece " << std::endl;
-	std::cout << "1. Spoutnik 1" << std::endl;
-	std::cout << "2. Explorer 4" << std::endl;
-	std::cout << "3. la-lune" << std::endl; // Bonne reponse
-	int reponse1;
-	std::cin >> reponse1;
-	if (reponse1 == 1) {
-		std::cout << " Bonne reponse ! " << std::endl;
-		score++;
+	while (score < 1) { // Cette boucle se répète tant que le score n'est pas supérieur ou égal à 1
+		// Question 1
+		std::cout << "\n Quel est le premier satellite envoyé dans l'espace ?" << std::endl;
+		std::cout << "1. Spoutnik 1" << std::endl;
+		std::cout << "2. Explorer 4" << std::endl;
+		std::cout << "3. la-lune" << std::endl; // Bonne réponse
+		int reponse1;
+
+		if (std::cin >> reponse1) {
+			if (reponse1 == 1) {
+				std::cout << "Bonne réponse !" << std::endl;
+				score++;
+			}
+			else if (reponse1 != 2 && reponse1 != 3) {
+				std::cout << "Mauvaise réponse.La bonne réponse est Spoutnik 1." << std::endl;
+			}
+		}
+		else {
+			std::cout << "Réponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+			std::cin.clear(); // Réinitialise l'état d'entrée
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore le reste de la ligne
+		}
 	}
-	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Spoutnik 1." << std::endl;
-	}
+
+	// Le reste du code du quizz continue ici.
+	// Vous pouvez poser d'autres questions ou gérer le score.
+
+
+	//// Question 1
+	//std::cout << "\n Quel est le premier satellite envoyer dans l'espece " << std::endl;
+	//std::cout << "1. Spoutnik 1" << std::endl;
+	//std::cout << "2. Explorer 4" << std::endl;
+	//std::cout << "3. la-lune" << std::endl; // Bonne reponse
+	//int reponse1;
+	//if (std::cin >> reponse1) {
+	//	if (reponse1 == 1) {
+	//		std::cout << "Bonne reponse !" << std::endl;
+	//		score++;
+	//	}
+	//	else if (reponse1 != 2 && reponse1 != 3) {
+	//		std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+	//	}
+	//	else {
+	//		std::cout << "Mauvaise reponse. La bonne reponse est Spoutnik 1." << std::endl;
+	//	}
+	//}
+	//else {
+	//	std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+	//	std::cin.clear(); // Réinitialise l'état d'entrée
+	//	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore le reste de la ligne
+	//	
+	//}
+
+
 
 	// Question 2
 	std::cout << "\n Question 2: Quelle est la capital de l'australie ?" << std::endl;
@@ -37,41 +79,71 @@ void Game1() {
 	std::cout << "2. Canberra " << std::endl;
 	std::cout << "3. Sydney" << std::endl;
 	int reponse2;
-	std::cin >> reponse2;
-	if (reponse2 == 2) {
-		std::cout << "Bonne reponse !" << std::endl;
-		score++;
+	if (std::cin >> reponse2) {
+		if (reponse2 == 1) {
+			std::cout << "Bonne reponse !" << std::endl;
+			score++;
+		}
+		else if (reponse2 != 2 && reponse2 != 3) {
+			std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		}
+		else {
+			std::cout << "Mauvaise reponse. La bonne reponse est Spoutnik 1." << std::endl;
+		}
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Canberra ." << std::endl;
+		std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		std::cin.clear(); // Réinitialise l'état d'entrée
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore le reste de la ligne
 	}
+
+
 	// Question 3
 	std::cout << "\n Question 3: Le jeu de combat le plus vendu au monde est fait par : " << std::endl;
 	std::cout << "1. Nintendo (developpeur de 'Mario')  " << std::endl; // Bonne reponse
 	std::cout << "2. Activision (developpeur de 'Call of Duty') " << std::endl;
 	std::cout << "3. EA(un des plus gros developpeurs de jeu au monde)" << std::endl;
 	int reponse3;
-	std::cin >> reponse3;
-	if (reponse3 == 1) {
-		std::cout << "Bonne reponse !" << std::endl;
-		score++;
+	if (std::cin >> reponse3) {
+		if (reponse3 == 1) {
+			std::cout << "Bonne reponse !" << std::endl;
+			score++;
+		}
+		else if (reponse3 != 2 && reponse3 != 3) {
+			std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		}
+		else {
+			std::cout << "Mauvaise reponse. La bonne reponse est Spoutnik 1." << std::endl;
+		}
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Nintendo ." << std::endl;
+		std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		std::cin.clear(); // Réinitialise l'état d'entrée
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore le reste de la ligne
 	}
+
 	// Question 4
 	std::cout << "\n Question 4: Quel acte de la vie courante occupe environ 6 mois de notre vie ? ?" << std::endl;
 	std::cout << "1. Attendre au feu rouge  " << std::endl; // Bonne reponse
 	std::cout << "2. Passer le balai " << std::endl;
 	std::cout << "3. Regarder la television" << std::endl;
 	int reponse4;
-	std::cin >> reponse4;
-	if (reponse4 == 1) {
-		std::cout << "Bonne reponse !" << std::endl;
-		score++;
+	if (std::cin >> reponse4) {
+		if (reponse4 == 1) {
+			std::cout << "Bonne reponse !" << std::endl;
+			score++;
+		}
+		else if (reponse4 != 2 && reponse4 != 3) {
+			std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		}
+		else {
+			std::cout << "Mauvaise reponse. La bonne reponse est Spoutnik 1." << std::endl;
+		}
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Attendre au feu rouge ." << std::endl;
+		std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		std::cin.clear(); // Réinitialise l'état d'entrée
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore le reste de la ligne
 	}
 
 	// Question 5
@@ -80,18 +152,30 @@ void Game1() {
 	std::cout << "2. les deux" << std::endl; // Bonne reponse
 	std::cout << "3. beton" << std::endl;
 	int reponse5;
-	std::cin >> reponse5;
-	if (reponse5 == 2) {
-		std::cout << "Bonne reponse !" << std::endl;
-		score++;
+	if (std::cin >> reponse5) {
+		if (reponse5 == 1) {
+			std::cout << "Bonne reponse !" << std::endl;
+			score++;
+		}
+		else if (reponse5 != 2 && reponse5 != 3) {
+			std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		}
+		else {
+			std::cout << "Mauvaise reponse. La bonne reponse est Spoutnik 1." << std::endl;
+		}
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est les deux." << std::endl;
+		std::cout << "Reponse non valide. Choisissez 1, 2 ou 3." << std::endl;
+		std::cin.clear(); // Réinitialise l'état d'entrée
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore le reste de la ligne
 	}
 
 	std::cout << "\nVotre score final est : " << score << " sur 5." << std::endl;
+	Replay();
 
 }
+
+//Theme 2, langage C
 
 void Game2() {
 	int score = 0; // Score initial
@@ -173,8 +257,11 @@ void Game2() {
 	}
 
 	std::cout << "\nVotre score final est : " << score << " sur 5." << std::endl;
-
+	Replay();
 }
+
+
+
 void Game3() {
 
 	int score = 0; // Score initial
@@ -183,53 +270,54 @@ void Game3() {
 	std::cout << "Repondez aux questions en choisissant la bonne option (1, 2 ou 3)." << std::endl;
 
 	// Question 1
-	std::cout << "\n  Qui est le personnage emblematique de la franchise de jeux Super Mario	 ? " << std::endl;
-	std::cout << "1. Mario" << std::endl;
-	std::cout << "2. Link" << std::endl;
-	std::cout << "3. Naruto" << std::endl; // Bonne reponse
+	std::cout << "\n Question 1: Lequel de ces personnages n'est pas issu de l'univers de Mario ? " << std::endl;
+
+	std::cout << "1. Peach" << std::endl;
+	std::cout << "2. Bowser" << std::endl;
+	std::cout << "3. Tail;" << std::endl; // Bonne reponse
 	int reponse1;
 	std::cin >> reponse1;
-	if (reponse1 == 1) {
+	if (reponse1 == 3) {
 		std::cout << " Bonne reponse ! " << std::endl;
 		score++;
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Mario." << std::endl;
+		std::cout << "Mauvaise reponse. La bonne reponse est Tails" << std::endl;
 	}
 
 	// Question 2
-	std::cout << "\n Question 2:  Quel jeu video populaire consiste a empiler des blocs pour creer des structures ?" << std::endl;
-	std::cout << "1. Roblox" << std::endl; // Bonne reponse
-	std::cout << "2. Tetris " << std::endl;
-	std::cout << "3. Minecraft" << std::endl;
+	std::cout << "\n Question 2: Quelle entreprise a cree la celèbre serie de jeux Pokemon?" << std::endl;
+	std::cout << "1.  l'entreprise japonaise Game Freak" << std::endl; // Bonne reponse
+	std::cout << "2. la societe  Game World Adventures." << std::endl;
+	std::cout << "3. l'entreprise  Monstres de Poche Inc" << std::endl;
 	int reponse2;
 	std::cin >> reponse2;
-	if (reponse2 == 3) {
+	if (reponse2 == 1) {
 		std::cout << "Bonne reponse !" << std::endl;
 		score++;
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Minecraft." << std::endl;
+		std::cout << "Mauvaise reponse. La bonne reponse est l'entreprise japonaise Game Freak." << std::endl;
 	}
 	// Question 3
-	std::cout << "\n Question 3: Quel jeu video de tir a la première personne met en scène des combats entre terroristes et antiterroristes ? " << std::endl;
-	std::cout << "1. Valorant  " << std::endl; // Bonne reponse
-	std::cout << "2. Conter-Strike" << std::endl;
-	std::cout << "3. Rainbow Six Siege" << std::endl;
+	std::cout << "\n Question 3: Quel jeu video populaire met en scène des animaux anthropomorphes vivant dans une ville et a ete developpe par Nintendo ? " << std::endl;
+	std::cout << "1. Super Mario World  " << std::endl; // Bonne reponse
+	std::cout << "2. Zelda's Animal Town" << std::endl;
+	std::cout << "3. Animal Crossing" << std::endl;
 	int reponse3;
 	std::cin >> reponse3;
-	if (reponse3 == 2) {
+	if (reponse3 == 3) {
 		std::cout << "Bonne reponse !" << std::endl;
 		score++;
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Conter-Strike." << std::endl;
+		std::cout << "Mauvaise reponse. La bonne reponse est Animal Crossing ." << std::endl;
 	}
 	// Question 4
-	std::cout << "\n Question 4: Quel jeu video de tir a la première personne vous place dans un conflit militaire fictif entre les etats-Unis et la Russie ?" << std::endl;
-	std::cout << "1. Call of Duty: Modern Warfare" << std::endl; // Bonne reponse
-	std::cout << "2. Halo" << std::endl;
-	std::cout << "3. Metal Gear Solid" << std::endl;
+	std::cout << "\n Question 4: Quel jeu video permet aux joueurs de construire des mondes virtuels avec des blocs de differentes couleurs ?" << std::endl;
+	std::cout << "1. Minecraft  " << std::endl; // Bonne reponse
+	std::cout << "2. Fortnite " << std::endl;
+	std::cout << "3. Grand Theft Auto V" << std::endl;
 	int reponse4;
 	std::cin >> reponse4;
 	if (reponse4 == 1) {
@@ -237,14 +325,14 @@ void Game3() {
 		score++;
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est Call of Duty: Modern Warfar." << std::endl;
+		std::cout << "Mauvaise reponse. La bonne reponse est Minecraft." << std::endl;
 	}
 
 	// Question 5
-	std::cout << "\nQuestion 5: Dans quel jeu video incarnez-vous un garçon nomme Link, qui doit sauver la princesse Zelda ?" << std::endl;
-	std::cout << "1. CookingMama" << std::endl;
-	std::cout << "2. MarioKart" << std::endl; // Bonne reponse
-	std::cout << "3. The Legend of Zelda" << std::endl;
+	std::cout << "\nQuestion 5: quel est le jeu de societe le plus ancien ?" << std::endl;
+	std::cout << "1. Monopoly  " << std::endl;
+	std::cout << "2. Jurassic Park" << std::endl; //Bonne reponse
+	std::cout << "3. Senet" << std::endl;
 	int reponse5;
 	std::cin >> reponse5;
 	if (reponse5 == 3) {
@@ -252,27 +340,25 @@ void Game3() {
 		score++;
 	}
 	else {
-		std::cout << "Mauvaise reponse. La bonne reponse est The Legend of Zelda." << std::endl;
+		std::cout << "Mauvaise reponse. La bonne reponse est Senet" << std::endl;
 	}
 
 	std::cout << "\nVotre score final est : " << score << " sur 5." << std::endl;
+	Replay();
 
 }
-
-
-
 void Theme() {
 	std::cout << "Choisissez un des themes qui sont presents ! \n" << std::endl;
 	std::cout << "   [1] Culture general" << std::endl;
 	std::cout << "   [2] Langage C " << std::endl;
 	std::cout << "   [3] Jeux video \n" << std::endl;
 
-	std::cout << "Choose an topic: ";
+	std::cout << "Choose an option: ";
 
 	int MenuChoice;
 	std::cin >> MenuChoice;
 
-	// Appelle a la fonction 
+	// Appelle à la fonction 
 
 	switch (MenuChoice)
 	{
@@ -288,32 +374,57 @@ void Theme() {
 		Game3();
 		break;
 
-	default :
+	default:
+		std::cout << "Good bye!" << std::endl;
 		break;
 	}
 
+}
+
+// Declarez cette fonction au debut de votre code pour la rendre accessible aux autres fonctions.
+void Replay() {
+	char choix;
+
+	printf("Do you want to replay ? (O/N) : ");
+	fflush(stdin);
+	scanf_s(" %c", &choix);
+
+	if (choix == 'O' || choix == 'o') {
+		// Si le joueur veut rejouer, appelez la fonction Game pour demarrer une nouvelle partie.
+		Theme();
+	}
+	else if (choix == 'N' || choix == 'n') {
+		printf("Thank for playing ! Good bye.\n");
+		//ExitApp(); // Vous pouvez egalement ajouter un appel à la fonction ExitApp pour quitter l'application ici.
+	}
+	else {
+		// Reponse non valide, redemandez.
+		printf("Answer incorrect. Please answer by Y(yes) or N(no)\n");
+		 // Appel recursif pour redemander la reponse.
+	}
 }
 
 
 // Fonction pour quitter le jeu
 void ExitApp() {
 	std::cout << "See you soon." << std::endl;
+	Replay();
 	exit(0);
 }
 
-// Fonction qui sert a guider le joueur
+// Fonction qui sert à guider le joueur
 
 void MainMenu() {
 	std::cout << "----- Main Menu -----" << std::endl;
 	std::cout << "   [1] Start the game" << std::endl;
 	std::cout << "           or " << std::endl;;
 	std::cout << "   [2] Leave the game \n" << std::endl;
-	std::cout << "Choose a option: ";
+	std::cout << "Choose an option: ";
 
 	int MenuChoice;
 	std::cin >> MenuChoice;
 
-	// Appelle a la fonction 
+	// Appelle à la fonction 
 
 	switch (MenuChoice)
 	{
